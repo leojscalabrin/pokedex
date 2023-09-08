65a4bba8953ad2179c8f4e13f304a6e67a827406
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-09-08 15:03:39 -0300

    pokemon list generator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,36 @@
-console.log("teste")
+const offset = 0;
+const limite = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?${offset}=0&limit=${limit}`;
+
+function convertPokemonToLi(pokemon) {
+  return `
+        <li class="pokemon">
+          <span class="number">#001</span>
+          <span class="name">${pokemon.name}</span>
+
+          <div class="detail">
+            <ol class="types">
+              <li class="type">Grass</li>
+              <li class="type">Poison</li>
+            </ol>
+
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+              alt="${pokemon.name}"
+            />
+          </div>
+        </li>
+        `;
+}
+const pokemonList = document.getElementsById('pokemonList')
+
+fetch(url)
+  .then((response) => response.json())
+  .then((jsonBody) => jsonBody.results)
+  .then((pokemons) => {
+     for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += convertPokemonToLi(pokemon)
+     }
+  })
+  .catch((error) => console.log(error));
