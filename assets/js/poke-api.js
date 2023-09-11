ba1cb38f4c9a439399b98ca3b55092c3103a5a62
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-09-11 14:00:42 -0300

    finish pokeAPI integracao

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,16 @@
 const pokeApi = {};
 
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json());
+};
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?${offset}=0&limit=${limit}`;
+
   return fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .catch((error) => console.log(error))
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailrequests) => Promise.all(detailrequests))
+    .then((pokemonsDetails) => pokemonsDetails)
 };
